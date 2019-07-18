05901e957dc3acd105c6b93cfcc9477614b7113d
AhmedOsama <32772828+AhmedOsamaen@users.noreply.github.com>
2019-07-18 18:03:38 +0200

    Add files via upload

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using NT_MVC_Task.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NT_MVC_Task.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            Customers c1 = new Customers() { id = 1, fname = "ahmed", lname="OSAMA" };
+           
+            return View(c1);
+        }
+    }
+}
---
+++ Controllers/MovieController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using NT_MVC_Task.Models;
+using System.Web.Mvc;
+
+namespace NT_MVC_Task.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Movie_Index()
+        {
+            Customers c1 = new Customers() { id = 1, fname = "ahmed", lname = "OSAMA" };
+            Customers c2 = new Customers() { id = 2, fname = "mhmd", lname = "hany" };
+            Customers c3 = new Customers() { id = 3, fname = "mhnd", lname = "hatem" };
+            List<Customers> c_list = new List<Customers> { c1, c2, c3 };
+            Movies spider_man = new Movies() { id = 1, name = "Spider Man", cust_list = c_list };
+            return View(spider_man);
+        }
+    }
+}
